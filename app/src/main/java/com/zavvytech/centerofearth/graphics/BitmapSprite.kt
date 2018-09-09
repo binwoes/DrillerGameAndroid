76c9bc074d748ea0496b09d37650aad9f8ee2c9f
Uwais Ashraf <uwaisash@gmail.com>
2018-09-09 01:09:22 +0100

    Add Sprite classes - unsure if Sprite interface is needed

---
+++ app/src/main/java/com/zavvytech/centerofearth/graphics/BitmapSprite.kt
@@ -0,0 +1,12 @@
+package com.zavvytech.centerofearth.graphics
+
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+
+class BitmapSprite(var texture: Texture, var paint: Paint): Sprite {
+
+    fun draw(canvas: Canvas, dst: RectF) {
+        canvas.drawBitmap(texture.bitmap, texture.rect, dst, paint)
+    }
+}
